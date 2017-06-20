5514d1eb3a711daac09819b14e59509cd1ba5a3f
Tom Kocik <tkocik@riis.com>
2017-06-20 14:41:36 -0400

    Updated MVC per requirements

--- app/src/main/kotlin/com.riis.simple.etaandroid/controllers/StopsController.kt
+++ app/src/main/kotlin/com.riis.simple.etaandroid/controllers/StopsController.kt
@@ -1,11 +1,13 @@
-package com.riis.simple.etaandroid.controllers.stop
+package com.riis.simple.etaandroid.controllers
 
 import com.riis.simple.etaandroid.callback.GetStopsCallback
+import com.riis.simple.etaandroid.model.StopModel
 import com.riis.simple.etaandroid.view.StopActivity
 
 class StopsController(val activity: StopActivity) : GetStopsCallback {
     fun getStops(companyNumber: Int, routeId: String, direction: String, daysActive: String) {
-        GetStopsAsyncTask(companyNumber, this).execute(routeId, direction, daysActive)
+        val model = StopModel(this)
+        model.getStops(companyNumber, routeId, direction, daysActive)
     }
 
     override fun onStart() {
--- app/src/main/kotlin/com.riis.simple.etaandroid/model/RouteModel.kt
+++ app/src/main/kotlin/com.riis.simple.etaandroid/model/RouteModel.kt
@@ -1,12 +1,43 @@
 package com.riis.simple.etaandroid.model
 
+import android.os.AsyncTask
+import com.riis.simple.etaandroid.callback.GetRoutesCallback
 import com.riis.simple.etaandroid.model.api.JsonFetcher
 import com.riis.simple.etaandroid.model.api.JsonParser
 import com.riis.simple.etaandroid.model.api.UrlStringBuilder
+import org.json.JSONException
+import java.io.FileNotFoundException
+import java.util.*
 
-class RouteModel {
-    fun getCompanyRoutes(companyNumber: Int) : List<Route> {
-        val routesJson = JsonFetcher.fetchUrl(UrlStringBuilder.getRoutesUrl(companyNumber))
-        return JsonParser.parseRoutes(routesJson)
+class RouteModel(val callback: GetRoutesCallback) {
+    fun getCompanyRoutes(companyNumber: Int) {
+        GetRoutesAsyncTask(callback).execute(companyNumber)
+    }
+
+    private inner class GetRoutesAsyncTask(val callback: GetRoutesCallback) : AsyncTask<Int, Void, List<Route>>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            callback.onStart()
+        }
+
+        override fun doInBackground(vararg params: Int?): List<Route>? {
+            if (params.count() != 1) {
+                return ArrayList()
+            }
+
+            try {
+                val routesJson = JsonFetcher.fetchUrl(UrlStringBuilder.getRoutesUrl(params[0]!!))
+                return JsonParser.parseRoutes(routesJson)
+            } catch (e: JSONException) {
+                return ArrayList()
+            } catch (e: FileNotFoundException) {
+                return ArrayList()
+            }
+        }
+
+        override fun onPostExecute(result: List<Route>) {
+            super.onPostExecute(result)
+            callback.onComplete(result)
+        }
     }
 }
