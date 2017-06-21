7c1bdb88c479cf9f09b6ac936e8a43a3db3dc493
Tom Kocik <tkocik@riis.com>
2017-06-21 09:45:35 -0400

    Update Build gradle

--- app/src/main/kotlin/com.riis.simple.etaandroid/viewmodel/interfaces/RouteViewModelInterface.kt
+++ app/src/main/kotlin/com.riis.simple.etaandroid/viewmodel/interfaces/RouteViewModelInterface.kt
@@ -3,8 +3,6 @@ package com.riis.simple.etaandroid.presenter.interfaces
 import com.riis.simple.etaandroid.model.Route
 
 interface RoutePresenter {
-    fun getRoutes(companyNumber: Int)
     fun onWebCallStart()
     fun onWebCallComplete(routeList : List<Route>)
-    fun onRouteRowClicked(route: Route)
 }
